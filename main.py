c4b1a2ed729088fa6d9687282f60ec8b5e66edd8
Gabriel Netto <biel_netto@hotmail.com>
2020-05-03 20:35:12 -0300

    Added audio conversor and audio to text

--- main.py
+++ main.py
@@ -4,28 +4,83 @@ import matplotlib.pyplot as plt
 from glob import glob
 import librosa as lr
 import base64
+import speech_recognition as sr
+from pydub import AudioSegment
+import os
+import argparse
 
-data_dir = 'audio_file'
-audio_files = glob(data_dir + '/*.wav')
 
-print(len(audio_files))
+def main():
+    data_dir = 'audio_file'
+    audio_files = glob(data_dir + '/*.wav')
+    # convertAudioFiles()
+    readAudio(audio_files)
+    transformToText(audio_files)
 
-audio, sfreq = lr.load(audio_files[1]) 
 
-print("audio file : ", audio_files[1])
+def convertAudioFiles():
+    formats_to_convert = ['.m4a']
+    for (dirpath, dirnames, filenames) in os.walk("audio_file/"):
+        for filename in filenames:
+            if filename.endswith(tuple(formats_to_convert)):
 
-time = np.arange(0,len(audio)) / sfreq
+                filepath = dirpath + '/' + filename
+                (path, file_extension) = os.path.splitext(filepath)
+                file_extension_final = file_extension.replace('.', '')
+                try:
+                    track = AudioSegment.from_file(filepath,
+                            file_extension_final)
+                    wav_filename = filename.replace(file_extension_final, 'wav')
+                    wav_path = dirpath + '/' + wav_filename
+                    print('CONVERTING: ' + str(filepath))
+                    file_handle = track.export(wav_path, format='wav')
+                    os.remove(filepath)
+                except:
+                    print("ERROR CONVERTING " + str(filepath))
 
-print("Time: ", time)
-print("Audio: ", audio)
-print("Máxima amplitude: ", max(audio), ", Ocorre no tempo ", list(time)[list(audio).index(max(audio))])
-print("Minima amplitude: ", min(audio), ", Ocorre no tempo ", list(time)[list(audio).index(min(audio))])
-print(min(audio))
+def readAudio(audio_files):
 
-print(len(time))
-print(len(audio))
+    print(len(audio_files))
 
-fig, ax = plt.subplots()
-ax.plot(time, audio)
-ax.set(xlabel="Tempo (s)", ylabel="Amplitude do Som")
-plt.show()
+    audio, sfreq = lr.load(audio_files[4]) 
+
+    print("audio file : ", audio_files[4])
+
+    time = np.arange(0,len(audio)) / sfreq
+
+    print("Time: ", time)
+    print("Audio: ", audio)
+    print("Máxima amplitude: ", max(audio), ", Ocorre no tempo ", list(time)[list(audio).index(max(audio))])
+    print("Minima amplitude: ", min(audio), ", Ocorre no tempo ", list(time)[list(audio).index(min(audio))])
+    print(min(audio))
+
+    print(len(time))
+    print(len(audio))
+
+    fig, ax = plt.subplots()
+    ax.plot(time, audio)
+    ax.set(xlabel="Tempo (s)", ylabel="Amplitude do Som")
+    plt.show()
+
+
+def transformToText(audio_files):
+    sound = "audio_file/record19.wav"
+    
+    r = sr.Recognizer()
+
+    with sr.AudioFile(sound) as source:
+        r.adjust_for_ambient_noise(source)
+        print("Converting Audio To Text ..... ")
+        audio = r.listen(source)
+    
+    try:
+        print("Converted Audio Is : \n" + r.recognize_google(audio, language = 'pt-PT'))
+
+    except Exception as e:
+        print(e)
+        print("Error: ".format(e))
+
+
+
+if __name__ == "__main__":
+    main()
