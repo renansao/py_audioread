5bdf77d8e8ac8ceb7027753d3033da2a52ceb90f
Renan Biagiotti <renan.biagiotti22@gmail.com>
2020-08-05 21:24:07 -0300

    Refactoring and commenting changes in the code.

--- conversorB64toFile.py
+++ conversorB64toFile.py
@@ -4,92 +4,47 @@ import io
 from pydub import AudioSegment
 import boto3
 
+#Retrieve ACCESS KEYS from enviroment
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-print(AWS_ACCESS_KEY_ID)
-print(AWS_SECRET_ACCESS_KEY)
-
+#Set S3 Configuration
 s3 = boto3.resource(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
 
+#Given encodedAudio and id's, save converted audio file to S3 bucket
 def saveAudioToS3(encodedAudio, username, audioId):
 
+    #Generate Audio Key (name for S3)
     audioKey = generateAudioKey(username, audioId, ".wav")
 
+    #Create M4A file base on B64 received in the request
     decodedAudio = base64.b64decode(encodedAudio)
     audioFile = io.BytesIO(decodedAudio)
 
+    #Create BytesIO to convert M4A File to WAV file
     wav = io.BytesIO()
+
+    #Convert audio using pydub.AudioSegment
     sound2 = AudioSegment.from_file(audioFile, "m4a")
     sound2.export(wav, format="wav")
 
+    #Search all buckets in account
     for bucket in s3.buckets.all():
+        #Save audio (.WAV) in the container with the unique generated name
         bucket.put_object(Key=audioKey, Body=wav.getvalue())
-        #bucket.put_object(Key="users/audiofileHEROKU.m4a", Body=audioFile.getvalue())
+    
+    #Close Buffers
     wav.close()
     audioFile.close()
+
     return
 
+#Generate Audio Key (name for S3)
 def generateAudioKey(username, audioId, extension):
 
     key = "user/" + "audio" + "/" + username + "/" + audioId + extension
     return key
-
-def conversorB64toFile(encodedAudio, audioId, username):
-
-    audioName = audioId + ".m4a"
-    path = "audio_file/"+ username
-    audioPath = "audio_file/"+ username + "/" + audioName
-
-    try:
-        os.open
-        try:
-            os.mkdir(path)
-        except OSError:
-            print ("Creation of the directory %s failed" % path)
-        else:
-            print ("Successfully created the directory %s " % path)
-        
-        wav_file = open(audioPath, "wb")
-        decodedAudio = base64.b64decode(encodedAudio)
-        wav_file.write(decodedAudio)
-
-        return audioPath, path
-    except Exception as e:
-        print(e)
-        return "Falha ao converter Base 64"
-
-def convertEncodedAudioToBytes(encodedAudio):
-
-    decodedAudio = base64.b64decode(encodedAudio)
-    audioFile = io.BytesIO(decodedAudio)
-
-    wav = io.BytesIO()
-    sound2 = AudioSegment.from_file(audioFile, "m4a")
-    sound2.export(wav, format="wav")
-
-    for bucket in s3.buckets.all():
-        bucket.put_object(Key="users/audiofileHEROKU.wav", Body=wav.getvalue())
-        #bucket.put_object(Key="users/audiofileHEROKU.m4a", Body=audioFile.getvalue())
-    wav.close()
-    audioFile.close()
-
-    return "ok"
-
-def convertM4AToWavBytes(m4aBytes):
-    try:
-        sound = AudioSegment.from_raw("audio_file/testeee22.m4a")
-        sound.export("audio_file/testeee2222.wav", format="wav")
-
-        sound2 = AudioSegment.from_file("audio_file/testeee22.m4a", "m4a")
-        sound2.export("audio_file/testeee3333.wav", format="wav")
-
-    except Exception as e:
-        print("Erro no service",e)
-        return "Falha na analise do audio"
-
-    return "yes"
