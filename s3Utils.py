e6e7e776f91cd55742a2ee9b5971d87085390660
Renan Biagiotti <renan.biagiotti22@gmail.com>
2020-08-06 10:50:13 -0300

    Chaning S3 resource to client

--- s3Utils.py
+++ s3Utils.py
@@ -6,7 +6,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 #Set S3 Configuration
-s3 = boto3.resource(
+s3 = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
